d26494f7d674067b78f7192fd0b05dbf3f8f37c7
Chloe Bloom <chloblo@unc.edu>
2026-09-09 13:04:35 -0400

    Add files via upload

---
+++ R Commands.R
@@ -0,0 +1,17 @@
+#this is an R script with all the running code
+
+x<-c(2,1,3) #naming a vector c() is a vector
+mean(x)# will find the mean of the named vector
+mean()# also can be written as sum()/length()
+sqrt() #square root
+<- #assinging something a value
+$ #pulls the variable out of the data set--- ex) wage 1 $ wage #pulls wage out
+summary() #quick snapshot of every variable in a data set    ex)min, mean, max
+var() #sample variance
+cov() #measures whether two variables tend to move together and in which dir
+cor ()#sample correlation--how to variables move together and in what direction
+sec() #creates a sequence of numbers
+: #a colon creates a sequence (e.g. 1:6 for a die
+rep() #repeats a value
+sum()#adds all the values together 
+sum(x*probability)#E(X) finds the expected value
